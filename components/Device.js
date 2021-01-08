5d29a30e60ba32de0056ec3b6cbda6ad31ddbf56
Tobias Claesson <tobbe.classon@hotmail.se>
2021-01-08 14:02:38 +0100

    removed wierd solution

--- components/Device.js
+++ components/Device.js
@@ -19,11 +19,11 @@ export default function Device(props) {
   };
 
   return (
-    <View style={styles.containerHelperWithSpaceBetween}>
-      <View style={styles.container}>
+    <View style={styles.container}>
+      <View style={styles.indicatorContainer}>
         <View style={isOn ? styles.greenIndicator : styles.redIndicator} />
-        <Text style={styles.deviceText}>{props.deviceName}</Text>
       </View>
+      <Text style={styles.deviceText}>{props.deviceName}</Text>
       <View style={styles.buttonContainer}>
         <Button title="On" onPress={() => onButtonClicked()} />
         <Button title="Off" onPress={() => offButtonClicked()} />
@@ -36,32 +36,32 @@ const styles = StyleSheet.create({
   container: {
     justifyContent: "flex-start",
     flexDirection: "row",
-  },
-  containerHelperWithSpaceBetween: {
-    justifyContent: "space-between",
-    flexDirection: "row",
     backgroundColor: "beige",
     alignItems: "center",
     height: 100,
     marginTop: 20,
   },
+  indicatorContainer: {
+    flex: 1,
+    marginLeft: 10,
+  },
   greenIndicator: {
     width: 20,
     height: 20,
     backgroundColor: "#42f548",
-    marginLeft: 10,
   },
   redIndicator: {
     width: 20,
     height: 20,
     backgroundColor: "red",
-    marginLeft: 10,
   },
   deviceText: {
+    flex: 6,
     fontSize: 18,
     marginLeft: 15,
   },
   buttonContainer: {
+    flex: 1,
     backgroundColor: "#fff",
     marginRight: 10,
   },
